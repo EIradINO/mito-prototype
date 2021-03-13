7ebcd8da1f97083c406d76b4d4b50479148f0445
EIradINO <eisukeinoue0107@gmail.com>
2021-03-13 19:36:14 +0900

    try janome

--- prototype/views.py
+++ prototype/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from janome.tokenizer import Tokenizer
+from janome.analyzer import Analyzer
+from janome.charfilter import *
+from janome.tokenfilter import *
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    li = []
+    text = "なんで空は青いの？"
+    char_filters = [UnicodeNormalizeCharFilter()]
+    tokenizer = Tokenizer()
+    token_filters = [POSKeepFilter(['名詞', '形容詞'])]
+    analyzer = Analyzer(char_filters=char_filters,
+                        tokenizer=tokenizer, token_filters=token_filters)
+    for token in analyzer.analyze(text):
+        li.append(token.surface + ',')
+    return HttpResponse(li)

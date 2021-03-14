e4010a7a3bd5c7d8ec719c90547bc01c563e1ade
EIradINO <eisukeinoue0107@gmail.com>
2021-03-15 08:50:32 +0900

    Add Scraping

--- prototype/urls.py
+++ prototype/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('scraping/', views.scraping, name='scraping'),
+    path('analysis', views.analysis, name='analysis'),
 ]
--- prototype/views.py
+++ prototype/views.py
@@ -3,10 +3,18 @@ from janome.analyzer import Analyzer
 from janome.charfilter import *
 from janome.tokenfilter import *
 from django.http import HttpResponse
+from django.template import loader
+import requests
+from bs4 import BeautifulSoup
 
 
 def index(request):
-    li = []
+    template = loader.get_template('prototype/index.html')
+    return HttpResponse(template.render())
+
+
+def analysis(request):
+    wordlist = []
     text = "なんで空は青いの？"
     char_filters = [UnicodeNormalizeCharFilter()]
     tokenizer = Tokenizer()
@@ -14,5 +22,17 @@ def index(request):
     analyzer = Analyzer(char_filters=char_filters,
                         tokenizer=tokenizer, token_filters=token_filters)
     for token in analyzer.analyze(text):
-        li.append(token.surface + ',')
-    return HttpResponse(li)
+        wordlist.append(token.surface)
+    template = loader.get_template('prototype/analysis.html')
+    context = {
+        'wordlist': wordlist,
+    }
+    return HttpResponse(template.render(context, request))
+    # return HttpResponse(wordlist)
+
+
+def scraping(request):
+    site = requests.get(
+        'https://www.nhk.or.jp/school/program/')
+    data = BeautifulSoup(site.content, 'lxml')
+    return HttpResponse(data.find('a').get('href'))

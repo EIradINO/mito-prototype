11b35e3e3befc2884c70c11bb282255e42409e29
EIradINO <eisukeinoue0107@gmail.com>
2021-03-29 15:30:24 +0900

    Scraping

--- prototype/views.py
+++ prototype/views.py
@@ -9,6 +9,7 @@ from bs4 import BeautifulSoup
 import time
 from selenium import webdriver
 import chromedriver_binary
+from collections import defaultdict
 
 
 def index(request):
@@ -17,7 +18,7 @@ def index(request):
 
 
 def analysis(request):
-    wordlist = []
+    word_list = []
     text = "なんで空は青いの？"
     char_filters = [UnicodeNormalizeCharFilter()]
     tokenizer = Tokenizer()
@@ -25,10 +26,10 @@ def analysis(request):
     analyzer = Analyzer(char_filters=char_filters,
                         tokenizer=tokenizer, token_filters=token_filters)
     for token in analyzer.analyze(text):
-        wordlist.append(token.surface)
+        word_list.append(token.surface)
     template = loader.get_template('prototype/analysis.html')
     context = {
-        'wordlist': wordlist,
+        'word_list': word_list,
     }
     return HttpResponse(template.render(context, request))
 
@@ -38,9 +39,15 @@ def scraping(request):
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     driver = webdriver.Chrome(options=options)
-
-    link_list = []
-    link_title_list = []
+    link_list = {}
+    # link_list = defaultdict(list)
+    # sample_list = defaultdict(list)
+    sample_list = {
+        'a': ['ai', 'au', 'ae', 'ao'],
+        'i': ['ii', 'iu', 'ie', 'io'],
+        'u': ['ui', 'uu', 'ue', 'uo'],
+        'e': ['ei', 'eu', 'ee', 'eo'],
+    }
 
     for i in range(1):
         url = 'https://www.nhk.or.jp/school/keyword/?kyoka=rika&grade=g5&cat=all&from={}&sort=ranking'.format(
@@ -48,12 +55,28 @@ def scraping(request):
         driver.get(url)
         html = driver.page_source.encode('utf-8')
         soup = BeautifulSoup(html, "html.parser")
+
         for link in soup.select('div.itemKyouka > a'):
-            link_list.append(link.get('href'))
+            mov_url = 'https:' + link.get('href')
+            driver.get(mov_url)
+            mov_html = driver.page_source.encode('utf-8')
+            mov_soup = BeautifulSoup(mov_html, "html.parser")
+
+            og_title = mov_soup.find(
+                'meta', attrs={'property': 'og:title', 'content': True})
+            og_description = mov_soup.find(
+                'meta', attrs={'property': 'og:description', 'content': True})
+            og_image = mov_soup.find(
+                'meta', attrs={'property': 'og:image', 'content': True})
+
+            link_list[mov_url] += [og_title['content'],
+                                   og_description['content'], og_image['content']]
+
+            time.sleep(1)
         time.sleep(1)
 
     template = loader.get_template('prototype/scraping.html')
     context = {
-        'linklist': link_list,
+        'link_list': link_list,
     }
     return HttpResponse(template.render(context, request))
